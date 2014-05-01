29124f166903365b9b0dd66cd5972f4bf7f991d5
skeldoy <skeldoy@gmail.com>
2014-05-01 23:51:27 +0200

    Update session.js

--- config/session.js
+++ config/session.js
@@ -15,7 +15,7 @@ module.exports.session = {
   // Session secret is automatically generated when your new app is created
   // Replace at your own risk in production-- you will invalidate the cookies of your users,
   // forcing them to log in again. 
-  secret: '958b68427b1393b47d220e85b261700a'
+  secret: '513c48427b1393b47d200e85b261730b'
 
 
   // In production, uncomment the following lines to set up a shared redis session store
